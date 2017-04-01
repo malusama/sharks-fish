cb5a57fa09fc1cf1d740a5079afad8bb69ddd1e3
malusama <chensichengmalu@gmail.com>
2017-04-01 20:47:52 +0800

    1
    1

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -2,65 +2,51 @@ package controller;
 import javax.swing.*;
 import java.awt.*;
 
-class DrawWindows extends JFrame{
-	
-	DrawWindows(){
-		
-		this.setTitle("Sharks And Fish"); //设置标题
-	    this.setSize(500, 500);  // 设置窗口大小
-	     //这是做什么的？
-	   // f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //这是做什么的？
-	      //设置为显示
-	    
-	    //Graphics graphics = this.getGraphics();
-	    
-	    //paint(graphics);
-	    
-	    
-	    this.setVisible(true);
-	    
-	    
-	}
-	public void paint(Graphics g){
-		g.setColor(Color.BLACK);
-		g.fillRect(0, 0, 200, 200);
-		
-	}
-	public void paint(Graphics g, SharksAndFish object){
-		
-		
-		
-	}
-	 
-}
-class SharksAndFish{
+
+class sea{
 	int width;
 	int height;
-	SharksAndFish(int width, int height){
+	sea(int width, int height){
 		this.width = width;
 		this.height = height;
 	}
+	
 }
-public class Main extends JPanel {
+public class Main extends JFrame {
+	
 	   public static void main(String[] a) {
-		   DrawWindows MainWindows = new DrawWindows();
-		   //Frame frame = new Frame("Sharks and Fish");
-		   //frame.setSize(500, 500);
-		  // frame.setVisible(true);
+		   
+		   JFrame frame = new JFrame("Sharks and Fish");
+		   frame.setSize(500, 500);
+		   sea Aa = new sea(100, 100);
+		   //JPanel panel = new JPanel();
+		  
+		   //frame.add(panel);
+		   //panel.setBackground(Color.WHITE);
+		   
+		   frame.setVisible(true);
+		   
+		   //Graphics graphics = panel.getGraphics();
+		   
+		  
+		   //drawsea(graphics,Aa);
+
+
 	   }
-	   public void paint(Graphics g,SharksAndFish sea) {
-		   g.setColor(Color.white);
-		   for(int i = 0;i < sea.height;i++)
-		   {
-			   for(int j = 0;j < sea.width;j++){
-				   
-				   g.fillRect (0, 0, 20, 20);
-				   
-			   }
-			   
-		   }
+	   
+	   public void paint(Graphics g){
+		   g.setColor(Color.black);
+		   //g.drawRect(0, 0, 100, 100);
+		   g.fillRect(0, 0, 100, 100);
 		   
 		   
 	   }
+	   private static void drawsea(Graphics g, sea object){
+		  g.setColor(Color.red);
+		 // g.drawRect(0, 0, 100, 100);
+		  g.fillRect(0, 0, 100, 100);
+			
+		}
+	 
 }
 
